b58973ed67163e9d76acc0c3cb01febc2435c615
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2023-09-19 20:59:47 +0600

    update

--- src/components/body/BodyComponent.js
+++ src/components/body/BodyComponent.js
@@ -5,9 +5,9 @@ import Photogalary from './Photogalary';
 const BodyComponent = () => {
     return (
         <div>
-           <h1>How are you</h1>
+
            <Photogalary />
-           <h1>hello</h1>
+
         </div>
     );
 };
